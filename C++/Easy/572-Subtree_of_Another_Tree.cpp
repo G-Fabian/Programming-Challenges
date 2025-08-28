1eebbc606bb5880ef48a88c8e9b6e3581fdaa3d1
G-Fabian <fabiangtech@gmail.com>
2025-08-27 22:30:59 -0700

    working on solution

--- C++/Easy/572-Subtree_of_Another_Tree.cpp
+++ C++/Easy/572-Subtree_of_Another_Tree.cpp
@@ -15,7 +15,50 @@
  */
 class Solution {
 public:
-    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+   // Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
+
+// A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants. The tree tree could also be considered as a subtree of itself.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    bool flag = false;
+
+    bool dfs(TreeNode* root, TreeNode* subRoot){
+
+        if(!root && subRoot)
+            return false;
         
+         if(root && !subRoot)
+            return false;
+
+
+        if(root && subRoot){
+            if(root->val == subRoot->val){
+                bool left  = dfs(root->left, subRoot->left);
+                bool right = dfs(root->right, subRoot->right);
+                return true;
+            }
+        }    
+
+        return false;
     }
+
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        bool flag = false;
+        flag = dfs(root, subRoot);
+        return flag;
+    }
+};
 };
