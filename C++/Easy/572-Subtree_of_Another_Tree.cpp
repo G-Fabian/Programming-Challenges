c8d4183af0f23a5a75659ab5a20a91314f479fd5
G-Fabian <fabiangtech@gmail.com>
2025-08-28 23:52:11 -0700

    solution for 572

--- C++/Easy/572-Subtree_of_Another_Tree.cpp
+++ C++/Easy/572-Subtree_of_Another_Tree.cpp
@@ -2,22 +2,6 @@
 
 // A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants. The tree tree could also be considered as a subtree of itself.
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-   // Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
-
-// A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants. The tree tree could also be considered as a subtree of itself.
 
 /**
  * Definition for a binary tree node.
@@ -32,33 +16,24 @@ public:
  */
 class Solution {
 public:
-
-    bool flag = false;
-// asd
-    bool dfs(TreeNode* root, TreeNode* subRoot){
-
-        if(!root && subRoot)
-            return false;
+    bool checkMatch(TreeNode* root, TreeNode* subRoot){
+        if(!root && !subRoot)
+            return true;
         
-         if(root && !subRoot)
+        if(!root || !subRoot)
             return false;
 
-
-        if(root && subRoot){
-            if(root->val == subRoot->val){
-                bool left  = dfs(root->left, subRoot->left);
-                bool right = dfs(root->right, subRoot->right);
-                return true;
-            }
-        }    
-
-        return false;
+        return (root->val == subRoot->val) &&
+               checkMatch(root->left, subRoot->left) &&
+               checkMatch(root->right, subRoot->right);
     }
 
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        bool flag = false;
-        flag = dfs(root, subRoot);
-        return flag;
+        if(!root)
+            return false;
+        
+        return checkMatch(root,subRoot) ||
+               isSubtree(root->left, subRoot) ||
+               isSubtree(root->right, subRoot);
     }
-};
 };
