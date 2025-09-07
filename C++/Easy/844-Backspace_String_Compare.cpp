314ec8a3646c05bac59992ed6608f8373420f2d2
G-Fabian <fabiangtech@gmail.com>
2025-09-06 23:02:30 -0700

    844 contstaints added

--- C++/Easy/844-Backspace_String_Compare.cpp
+++ C++/Easy/844-Backspace_String_Compare.cpp
@@ -1,6 +1,11 @@
 // Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
 // Note that after backspacing an empty text, the text will continue empty.4
 
+// Constraints:
+
+// 1 <= s.length, t.length <= 200
+// s and t only contain lowercase letters and '#' characters.
+
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
