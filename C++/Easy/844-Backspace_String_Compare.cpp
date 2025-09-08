22a1f1a632a2ad381727ac7c6ac9e5a5004886b1
G-Fabian <fabiangtech@gmail.com>
2025-09-07 23:18:16 -0700

    current solution #844

--- C++/Easy/844-Backspace_String_Compare.cpp
+++ C++/Easy/844-Backspace_String_Compare.cpp
@@ -10,5 +10,48 @@ class Solution {
 public:
     bool backspaceCompare(string s, string t) {
         
+        char* ptr1 = nullptr;
+        char* ptr2;
+
+        s = eraseIt(s);
+        t = eraseIt(t);
+
+        cout<<s<<" "<<t<<endl;
+
+        if(s==t) return true;
+        else return false;
     }
+
+    string eraseIt(string s){
+        
+        for(int i=0; i<s.size()-1; i++){
+
+            if(s[i+1]=='#'){
+                s.erase (i,2);
+            }
+        }
+        
+        return s;
+    }
+
 };
+
+
+// s = "a#c", t = "b"
+
+// a #
+// c end
+// return c
+
+
+// b end
+// return b
+
+// (ab)##
+// a(b#)#
+// (a#)
+// return nothing
+
+// (c#)d#
+// (d#)
+// return nothing
