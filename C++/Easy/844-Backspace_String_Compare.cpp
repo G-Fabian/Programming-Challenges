1a8c5d21848ec96d3497b5420c85cbd5fcdedadb
G-Fabian <fabiangtech@gmail.com>
2025-09-15 22:50:22 -0700

    final solution finally

--- C++/Easy/844-Backspace_String_Compare.cpp
+++ C++/Easy/844-Backspace_String_Compare.cpp
@@ -1,41 +1,50 @@
+
+
+// Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
+// Note that after backspacing an empty text, the text will continue empty.
+// **Note: challanging one practice another time
+
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        
+
+        // Obtain lengths of strings after alteration        
         int k = backspace(s);
-
-        cout<<s<<endl;
-
         int p = backspace(t);
 
-        cout<<t<<endl;
-
+        // Length do not match
         if (k != p) return false;
 
+        // Find discrepancies between the two strings
         for (int i = 0; i < k; i++) {
             if (s[i] != t[i]) return false;
         }
 
+        // Found match
         return true;
     }
-
+    
+    
+    // Modify previous if there is a '#'
+    // instead of deleting the character we can 
+    // alter strings such that if 2 strings are 
+    // similar they should have the same alteration
     int backspace(string& s){
-
-        int k = 0;
-
         /*
-        whole string: ab#c
-        a k=0, k=1
-        ab k=1, k=2
-        ab# k=2, k=1
-        ac#
-
+            whole string: ab#c
+            a k=0, k=1
+            ab k=1, k=2
+            ab# k=2, k=1
+            ac#
         */
 
+        int k = 0;
         for(char c:s){
             if(c!='#'){
+                // Add c to s[k] then increment
                 s[k++] = c;
             } else if (k>0){
+                // Go back one char and modify if not #
                 k--;
             }
         }
@@ -46,22 +55,14 @@ public:
 };
 
 
-// s = "a#c", t = "b"
-
-// a #
-// # c
-// c end
-// return c
 
 
-// b end
-// return b
 
-// ab##
-// ab##
-// 
-// return nothing
 
-// (c#)d#
-// (d#)
-// return nothing
+
+
+
+
+
+
+
